a2b3d3c38fd004e27556df605fbc4b408fef3f0c
prefer_youn <shyoun94@gmail.com>
2023-06-10 17:35:29 +0900

    Style: 로그인 회원가입 페이지
    Style: 로그인 회원가입 페이지

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Splash from "./components/Splash/Splash";
 import styled from "styled-components";
 import RecommendList from "./components/Profile/RecommendList";
 import Header from "./components/common/Header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/AuthorPage/Login/Login";
+import SignUp from "./pages/AuthorPage/SignUp/SignUp";
 
 const Container = styled.div`
   max-width: 390px;
@@ -14,11 +17,21 @@ function App() {
   return (
     <Container>
       <GlobalStyle />
-      <Header type="profile" />
+      {/* <Header type="profile" />
       <ProfileInformation />
       <RecommendList />
-      <Splash />
+      <Splash /> */}
+      <Login />
+      <SignUp />
     </Container>
+    // <>
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route path="/" element={<Login />} />
+    //       <Route path="/signup" element={<SignUp />} />
+    //     </Routes>
+    //   </BrowserRouter>
+    // </>
   );
 }
 export default App;
