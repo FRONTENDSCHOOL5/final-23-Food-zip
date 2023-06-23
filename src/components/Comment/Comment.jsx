bf3a89645dac1a377041dac617ef8ab25fdc84b7
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-06-23 16:35:57 +0900

    Fix: CommentList 높이 문제 수정

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MoreIcon from "../../assets/images/icon-more-vertical.svg";
+import Alert from "../Modal/Alert";
+import Modal from "../Modal/Modal";
+
 const StyledCommentWrapper = styled.ul`
-  height: 287px;
+  max-height: 287px;
 `;
 const StyledComment = styled.li`
   position: relative;
@@ -59,33 +62,82 @@ const CommentContent = styled.p`
   margin: 10px 0;
 `;
 
-export default function Comment({ commentList, modalOpen }) {
-  console.log("댓글정보 !", commentList);
+export default function Comment({ commentList, postId }) {
+  const where = localStorage.getItem("accountname");
+  console.log(commentList);
+  const [modalShow, setModalShow] = useState(false);
+  const [modalType, setModalType] = useState("delete");
+  const [selectedId, setSelectedId] = useState(null);
+  function modalClose(e) {
+    if (e.target === e.currentTarget) {
+      setModalShow(false);
+    }
+  }
+  function modalOpen(type, id) {
+    console.log("type", type);
+    setModalShow(true);
+    setModalType(type);
+    setSelectedId(id);
+  }
+  console.log(commentList);
+  console.log("commentId", selectedId);
+
+  const [alertShow, setAlertShow] = useState(false);
+  function alertClose(e) {
+    if (e.target === e.currentTarget) {
+      setAlertShow(false);
+    }
+  }
+
+  function alertOpen() {
+    setAlertShow(true);
+  }
+
   return (
     <StyledCommentWrapper>
       {commentList?.map(comment => {
         return (
-          <>
-            <StyledComment key={comment.id}>
-              <CommentUserProfile
-                src={comment.author.image}
-                alt="유저의 프로필"
-              />
-              <StyledCommentContent>
-                <StyledCommentUserInfo>
-                  <h3>{comment.author.username}</h3>
-                  <p>몇분전</p>
-                </StyledCommentUserInfo>
-                <CommentContent>{comment.content}</CommentContent>
-              </StyledCommentContent>
-              <CommentBtnMore
-                type="button"
-                onClick={modalOpen}
-              ></CommentBtnMore>
-            </StyledComment>
-          </>
+          <StyledComment key={comment.id}>
+            <CommentUserProfile
+              src={comment.author.image}
+              alt="유저의 프로필"
+            />
+            <StyledCommentContent>
+              <StyledCommentUserInfo>
+                <h3>{comment.author.username}</h3>
+                <p>몇분전</p>
+              </StyledCommentUserInfo>
+              <CommentContent>{comment.content}</CommentContent>
+            </StyledCommentContent>
+            <CommentBtnMore
+              type="button"
+              onClick={() => {
+                modalOpen(
+                  where === comment.author.accountname ? "delete" : "report",
+                  comment.id,
+                );
+              }}
+            ></CommentBtnMore>
+          </StyledComment>
         );
       })}
+      {modalShow && (
+        <Modal
+          type={modalType}
+          modalClose={modalClose}
+          alertOpen={alertOpen}
+          commentId={selectedId}
+          postId={postId}
+        />
+      )}
+      {alertShow && (
+        <Alert
+          type="comment"
+          alertClose={alertClose}
+          commentId={selectedId}
+          postId={postId}
+        />
+      )}
     </StyledCommentWrapper>
   );
 }
