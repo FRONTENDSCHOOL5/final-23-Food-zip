a164d5c88b0c4b8840de5d0bd92f2bd9a47fba32
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-07-11 15:28:23 +0900

    Refactor: 스타일 분리, 시맨틱 태그로 수정

---
+++ src/pages/Profile/ProfileEdit/ProfileEditStyle.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 48px;
+  gap: 12px;
+  background: #fff;
+  height: 100vh;
+`;
+
+export default Container;
--- src/pages/ProfileSetting/ProfileSetting.jsx
+++ src/pages/ProfileSetting/ProfileSetting.jsx
@@ -1,22 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 import ProfileForm from "../../components/Profile/ProfileForm/ProfileForm";
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 30px;
-  gap: 12px;
-  background: #fff;
-  height: 100vh;
-`;
-const Title = styled.h1`
-  font-size: 24px;
-`;
-const SubText = styled.p`
-  font-size: 14px;
-  color: #767676;
-`;
+import { Container, Title, SubText } from "./ProfileSettingStyle";
 
 export default function ProfileSetting() {
   return (
