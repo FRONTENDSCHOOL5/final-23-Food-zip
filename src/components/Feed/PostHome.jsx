7590046425f8482737096101fabf421300b095ad
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-07-29 02:56:51 +0900

    Refactor: PostHome recoil로 상태 관리

--- src/components/Feed/PostHome.jsx
+++ src/components/Feed/PostHome.jsx
@@ -3,26 +3,36 @@ import PostItem from "../Post/PostItem/PostItem";
 import styled from "styled-components";
 import Modal from "../Modal/Modal/Modal";
 import { feed } from "../../api/post";
+import { useRecoilState } from "recoil";
+import { modalState } from "../../atoms/modalAtom";
 
 const List = styled.ul`
   background-color: white;
   padding: 57px 24px 69px 24px;
 `;
 
-export default function PostHome({ myFeed, modalOpen, authorInfo }) {
-  const [modalShow, setModalShow] = useState(false);
-  const [selectedId, setSelectedId] = useState(null);
+export default function PostHome({ myFeed, authorInfo }) {
+  // const [modalShow, setModalShow] = useState(false);
+  // const [selectedId, setSelectedId] = useState(null);
   const [otherInfo, setOtherInfo] = useState(myFeed);
-  function modalClose(e) {
-    if (e.target === e.currentTarget) {
-      setModalShow(false);
-    }
-  }
+  // function modalClose(e) {
+  //   if (e.target === e.currentTarget) {
+  //     setModalShow(false);
+  //   }
+  // }
 
-  function modalOpen(id) {
-    setSelectedId(id);
-    setModalShow(true);
-  }
+  // function modalOpen(id) {
+  //   setSelectedId(id);
+  //   setModalShow(true);
+  // }
+  const [modal, setModal] = useRecoilState(modalState);
+  console.log(modal);
+  const modalOpen = id => {
+    setModal({
+      show: true,
+      postId: id,
+    });
+  };
   const getOtherInfo = async () => {
     const token = localStorage.getItem("token");
     const res = await feed(token);
@@ -41,9 +51,7 @@ export default function PostHome({ myFeed, modalOpen, authorInfo }) {
             />
           </li>
         ))}
-        {modalShow && (
-          <Modal type="report" modalClose={modalClose} postId={selectedId} />
-        )}
+        {modal.show && <Modal type="report" />}
       </List>
     </main>
   );
